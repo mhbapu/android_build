4303da8c76615020e86b77874d519a9bac49ab1a
Dave Patel <davempatel@gmail.com>
2017-11-12 18:32:33 -0800

    Added bin,experiment data and screenshot directory

---
+++ bin/setup_env.sh
@@ -0,0 +1,11 @@
+
+PATH=/mnt/irp210/dave/bin:$PATH
+
+
+# Android-related configs below
+# Java 8 for Oreo/Marshmellow
+#export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+# Java 7 for Lollipop
+#export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
